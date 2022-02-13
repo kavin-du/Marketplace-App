67423518072473340f42a28024ea0deb56f748bf
Kavindu Chamith <kavindu.chamith@pagero.com>
2022-02-13 15:36:04 +0530

    welcome screen added

--- App.tsx
+++ App.tsx
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet, Text, Image, View, StatusBar,
+  TouchableHighlight,
+  Button,
+  Alert,
+  SafeAreaView,
+  Platform,
+
+} from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const handlePress = () => console.log("button pressed");
+  
+  return <WelcomeScreen />;
+
+  // return (
+  //   // safe area view only for iOs, in andriod need padding
+  //   <SafeAreaView style={[styles.container, containerStyle]}>
+  //     <View style={{
+  //       backgroundColor: 'dodgerblue',
+  //       width: '50%',
+  //       height: 70,
+  //     }}></View>
+  //     <Text numberOfLines={1} onPress={handlePress}>
+  //       Open up App.tsx to start working on your app! Open up App.tsx to start
+  //       working on your app!Open up App.tsx to start working on your app
+  //     </Text>
+  //     <Image source={require("./assets/favicon.png")} />
+  //     <TouchableHighlight>
+  //       <Image source={{
+  //         uri: "https://picsum.photos/200/300",
+  //         width: 100,
+  //         height: 100,
+  //       }} />
+  //     </TouchableHighlight>
+  //     <Button title="press me" onPress={() => Alert.alert('My title', 'My message', [
+  //       { text: 'Yes', onPress: () => console.log('yes') },
+  //       { text: 'No', onPress: () => console.log('No') },
+  //     ])} />
+
+  //     <Button title="press me2" onPress={() => Alert.prompt('title', 'message', (text) => console.log(text))} />
+  //     {/* <StatusBar style="auto" /> */}
+  //   </SafeAreaView>
+  // );
 }
+
+const containerStyle = { backgroundColor: 'orange' };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
---
+++ app/screens/WelcomeScreen.tsx
@@ -0,0 +1,44 @@
+import { Button, Image, ImageBackground, Platform, SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
+
+export default function WelcomeScreen() {
+
+    return (
+        <ImageBackground source={require('../assets/background.jpg')} style={styles.background} >
+            <View style={styles.logoContainer}>
+                <Image style={styles.logo} source={require('../assets/logo-red.png')} />
+                <Text>Sell what you don't need</Text>
+            </View>
+            <View style={styles.loginButton}></View>
+            <View style={styles.registerButton}></View>
+        </ImageBackground>
+
+    );
+}
+
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1, // takes entire screen
+        justifyContent: 'flex-end', // align along primary axis, buttons
+        alignItems: 'center', // align along secondary axis, the logo
+    },
+    loginButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: '#fc5c65'
+    },
+    logo: {
+        width: 100, 
+        height: 100,
+    },
+    logoContainer: {
+        position: 'absolute',
+        top: 70,
+        alignItems: 'center',
+    },
+    registerButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: '#4ECDC4'
+    }
+});
